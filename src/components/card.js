fdb0b415da7dd3f4adb70b294fd525d895069c24
Fernando Velasco <fervelgo@gmail.com>
2021-05-07 19:45:27 -0500

    restarting with cards, current approach did not work

--- src/components/card.js
+++ src/components/card.js
@@ -1,26 +1,5 @@
 import axios from "axios";
 
-const testData = [
-  {
-    id: '5efa1378-c91f-49eb-9955-02e477c95920',
-    headline: 'Bootstrap 5: Get a Sneak Peak at all the New Features',
-    authorPhoto: 'https://tk-assets.lambdaschool.com/1891c758-b3f4-4ec7-9d88-590bf7c7ceb2_fido.jpg',
-    authorName: 'FIDO WALKSALOT'
-  },
-  {
-    id: 'd03affc6-b06e-4b4a-9909-3981c7cb8389',
-    headline: 'UI Frameworks: A Comparison, Which Made Our List? (Hint: Bootstrap is on it)',
-    authorPhoto: 'https://tk-assets.lambdaschool.com/247dd20f-7e32-479b-a861-c3bbe85b5f0c_max.jpg',
-    authorName: 'MAX GOODBOYE'
-  },
-  {
-    id: 'a85f88d3-0845-426d-9f02-930daa5c9b34',
-    headline: 'The Hottest New Bootstrap 4 Components Every Developer Needs to Know About',
-    authorPhoto: 'https://tk-assets.lambdaschool.com/247dd20f-7e32-479b-a861-c3bbe85b5f0c_max.jpg',
-    authorName: 'MAX GOODBOYE'
-  }
-]
-;
 
 const Card = (article) => {
   // TASK 5
@@ -42,7 +21,7 @@ const Card = (article) => {
   // </div>
   //
 
-  article.forEach((item)=>{
+
     const cardHolder = document.createElement('div');
     const headline = document.createElement('div');
     const author = document.createElement('div');
@@ -66,32 +45,12 @@ const Card = (article) => {
     // authorName.textContent = 'By ' + item.authorName;
 
     return cardHolder;
-  });
+  }
 
-}
+
 
 const cardAppender = (selector) => {
-  const cardCont = document.querySelector(selector);
-  console.log(Card(testData));
-  //axios
-  // .get('https://lambda-times-api.herokuapp.com/articles')
-  // .then((res)=>{
-  //   const data = res.data.articles;
-  //   for(const keys in data){
-  //         const keyData = data[keys];
-  //     console.log(`${keys}`, keyData);
-  //         //debugger;
-  //     //cardCont.appendChild(Card(keyData))
-  //     //data[keys].forEach((cb)=>{
-        
-  //      // });
-  //   }
-  //   //cardCont.appendChild(Card(res.data.articles)); //pass the whole nested objects and then dig in in the for loop
-  // })
-  // .catch((err)=>{
-  //   console.log(err);
-  // })
-  
+
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
