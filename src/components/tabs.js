b1fb4b99fbf5cf167d28574f9c80b61df88489d6
Fernando Velasco <fervelgo@gmail.com>
2021-05-07 19:26:50 -0500

    m

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -37,7 +37,6 @@ const tabsAppender = (selector) => {
   axios
   .get('https://lambda-times-api.herokuapp.com/topics')
   .then((res) => {
-    console.log(res);
     tabsCont.appendChild(Tabs(res.data.topics));
   })
   .catch((err) =>{
